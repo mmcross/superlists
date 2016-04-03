fc552a8c26c19e7fe45fdfd8aec94df54e0ffcba
mmcross <y113355@yeah.net>
2016-04-03 17:04:43 +0800

    model for list items

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 from django.template.context_processors import csrf
+from lists.models import Item
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -15,7 +16,11 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		expected_html = render_to_string('home.html')
+		c = {}
+		c.update(csrf(request))
+		expected_html = render_to_string(
+			'home.html',c
+			)
 		self.assertEqual(response.content.decode(),expected_html)
 
 	def test_home_page_can_save_a_POST_request(self):
@@ -32,3 +37,22 @@ class HomePageTest(TestCase):
 			'home.html',c
 			)
 		self.assertEqual(response.content.decode(),expected_html)
+
+
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		first_saved_item = second_item[0]
+		second_saved_item = second_item[1]
+		self.assertEqual(first_saved_item,'The first (ever) list item')
+		self.assertEqual(second_saved_item,'Item the second')
