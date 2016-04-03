0bc6cccac65202383cfeab99d0d90d179d137185
mmcross <y113355@yeah.net>
2016-04-03 12:38:08 +0800

    before rebuild

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
+from django.template.context_processors import csrf
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -13,6 +15,20 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html'))
-		self.assertIn(b'<title>To-Do lists</title>',response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(),expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+		self.assertIn('A new list item',response.content.decode())
+		c = {}
+		c.update(csrf(request))
+		c['new_item_text']='A new list item'
+		expected_html = render_to_string(
+			'home.html',c
+			)
+		self.assertEqual(response.content.decode(),expected_html)
