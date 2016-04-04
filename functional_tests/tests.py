667fdd82ad066a11c6f75bc8eec8fb54c216a873
mmcross <y113355@yeah.net>
2016-04-04 12:56:59 +0800

    first finish

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -65,3 +65,6 @@ class NewVisitorTest(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers',page_text)
 		self.assertNotIn('make a fly',page_text)
+
+
+
