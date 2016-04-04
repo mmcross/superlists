6651709479ac44dbe8ab01bc9f5aa0d858af26a9
mmcross <y113355@yeah.net>
2016-04-04 14:51:49 +0800

    move database

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,23 @@
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url='http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -18,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn(row_text,[row.text for row in rows])		
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 
 		self.assertIn('To-Do',self.browser.title)
@@ -51,7 +66,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser.quit()
 		self.browser = webdriver.Chrome()
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers',page_text)
 		self.assertNotIn('make a fly',page_text)
@@ -69,7 +84,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertNotIn('make a fly',page_text)
 
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from lists import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^lists/',include('lists.urls')),
     url(r'^$',views.home_page,name='home')
 ]
