9dc2398bc0c9d4d42c5f25a700b75331429d9e15
mmcross <y113355@yeah.net>
2016-04-14 12:20:40 +0800

    simplify tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,23 +6,20 @@ from django.template.loader import render_to_string
 from django.template.context_processors import csrf
 from lists.models import Item,List
 from django.utils.html import escape
+from lists.forms import ItemForm
 # Create your tests here.
 
 class HomePageTest(TestCase):
+	
+	def test_home_page_renders_home_template(self):
+	    response = self.client.get('/')
+	    self.assertTemplateUsed(response, 'home.html')
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func,home_page)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		c = {}
-		c.update(csrf(request))
-		expected_html = render_to_string(
-			'home.html',c
-			)
-		self.assertEqual(response.content.decode(),expected_html)
+	def test_home_page_uses_item_form(self):
+	    response = self.client.get('/')
+	    self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class ListViewTest(TestCase):
 	def test_uses_list_template(self):
