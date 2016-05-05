3246beb5cf2c8902e160e9566838f2741d33f408
mmcross <y113355@yeah.net>
2016-05-05 14:55:52 +0800

    second finish

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,25 +3,55 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('.has-error')
+
 	def test_cannot_add_empty_list_items(self):
 		self.browser.get(self.server_url)
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')\
+		self.get_item_input_box().send_keys('\n')
 
-		error = self.browser.find_element_by_css_selector('.has-error')	
+		error = self.get_error_element()
 		self.assertEqual(error.text,"You can't have an empty list item")
 
-		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+		self.get_item_input_box().send_keys('Buy milk\n')
 
 		
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url,'/lists/.+')
 		self.check_for_row_in_list_table('1: Buy milk')
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		self.check_for_row_in_list_table('1: Buy milk')
-		error = self.browser.find_element_by_css_selector('.has-error')	
+		error = self.get_error_element()
 		self.assertEqual(error.text,"You can't have an empty list item")
 
-		self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+		self.get_item_input_box().send_keys('Make tea\n')
+	@skip
+	def test_cannot_add_duplicate_items(self):
+		self.browser.get(self.server_url)
 
+		self.get_item_input_box().send_keys('Buy wellies')
+		self.get_item_input_box().send_keys('\n')
+		self.check_for_row_in_list_table('1: Buy wellies')
 
+		self.get_item_input_box().send_keys('Buy wellies')
+		self.get_item_input_box().send_keys('\n')
+
+		self.check_for_row_in_list_table('1: Buy wellies')
+		error = self.get_error_element()
+		self.assertEqual(error.text,"You've already got this in your list")
+
+	@skip
+	def test_error_messages_are_cleared_on_input(self):
+		# Edith starts a new list in a way that causes a validation error:
+		self.browser.get(self.server_url)
+		self.get_item_input_box().send_keys('\n')
+		error = self.get_error_element()
+		self.assertTrue(error.is_displayed())
+
+		# She starts typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
+
+		# She is pleased to see that the error message disappears
+		error = self.get_error_element()
+		self.assertFalse(error.is_displayed())
--- lists/views.py
+++ lists/views.py
@@ -2,35 +2,29 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from lists.models import Item,List 
 from django.core.exceptions import ValidationError
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 
 # Create your views here.
 
 def home_page(request):
 	return render(request,'home.html',{'form':ItemForm()})
 
-def view_list(request,list_id):
-	list_ = List.objects.get(id=list_id)
-	error = None
-
-	if request.method == 'POST':
-		try:
-			item = Item.objects.create(text=request.POST['item_text'],list=list_)
-			item.full_clean()
-			item.save()
-			return redirect(list_)
-		except ValidationError:
-			error = "You can't have an empty list item"
-	return render(request,'list.html',{'list':list_,'error':error})
-
 def new_list(request):
-	list_ = List.objects.create()
-	item = Item.objects.create(text=request.POST['item_text'],list=list_)
-	try:
-		item.full_clean()
-		item.save()
-	except ValidationError:
-		list_.delete()
-		error = "You can't have an empty list item"
-		return render(request,'home.html',{'error':error})
-	return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
